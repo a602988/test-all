dd67fbe1e65773b2f51eb2e787e8529fb6ce8725
a602988 <a680603@gmail.com>
2023-12-03 22:09:40 +0800

    1
    1

--- myweb/pages/api/hello.js
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({
-    name: 'Jean 來囉',
-    youtube: 'jean學習'
-  })
-}
